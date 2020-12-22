540771e54a1ac3ae224e9fb681088732d58af901
KirbyPaint <porter.ashley13@gmail.com>
2020-12-22 14:33:20 -0800

    renamed function process() to function arrayify() to reflect function's new purpose. Function arrayify() passes array creation test.

--- README.md
+++ README.md
@@ -16,9 +16,9 @@ _The purpose of this website is to demonstrate knowledge of arrays and looping._
 
 _TEMPLATE SPECS BELOW TO BE FILLED OUT AS TESTS PASS_
 
-_Describe: process()_
-_Test: "Current test: iteration"_
-_Expect(beepBoop(10).toEqual([10]);_
+_Describe: arrayify()_
+_Test: "Function will take user input and return input as an array containing all digits from 0 to the number submitted by the user."_
+_Expect(arrayify(10).toEqual([0,1,2,3,4,5,6,7,8,9,10]);_
 _Status: Passed_
 
 ## Known Bugs
--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,30 @@
-function process(variable) {
-  const testVar = variable;
-  for (let i = 0; i < testVar; i++) {
-    console.log(i);
-    console.log(testVar);
-    //return i; //to test the incrementation alone
+// I think the plan of attack here is to take the number, and for each time it iterates, add the number to the array.
+// Have a function specifically dedicated to breaking apart the numbers in the array
+// For example: it would take 11 and in the array, that would be [1, 1]
+
+//Step one: for loop to add numbers to the array
+//Then output the numbers
+//Step One Complete
+
+//Step Two: take the numbers in the array and separate them into individual digits
+//Then recombine as a full array again.
+
+//This function's purpose is only to create an array containing the numbers that make up the user-provided number
+//Rename to arrayify
+function arrayify(variable) {
+  const userInputTransform = variable;
+  let numbersArray = []; // Blank array to hold the digits
+  for (let i = 0; i <= userInputTransform; i++) {
+    numbersArray.push(i);
   }
-  return testVar;
+  return numbersArray;
 }
 
 $(document).ready(function() {
   $("form#process").submit(function(event) {
     event.preventDefault();
-      const input = parseInt($("#userInputForm").val());
-      const result = process(input);
+      const userInput = parseInt($("#userInputForm").val());
+      let result = arrayify(userInput);
       $("#output").text(result);
       $("#output").show();
   });
